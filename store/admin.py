ae542a02e38dde12ce364d8f3540f387f09ac778
Fkill D Azad <69202230+fkilld@users.noreply.github.com>
2023-06-13 09:51:31 +0530

    7

---
+++ store/admin.py
@@ -0,0 +1,14 @@
+from .models import Product
+from django.contrib import admin
+
+# Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'price', 'stock',
+                    'category', 'modified_date', 'is_available')
+    prepopulated_fields = {'slug': ('product_name',)}
+    
+
+
+admin.site.register(Product, ProductAdmin)
